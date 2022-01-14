88eb87136978062ab2dc9040d1704cb47aba95ad
phuongtd158 <tdphuong2002@gmail.com>
2022-01-15 00:41:46 +0700

    commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+//wow
+$(function(){
+    new WOW().init();
+})
+
+//set background for navbar when scroll
+const nav = document.querySelector('.navbar');
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add('header-scroll');
+    }else{
+        nav.classList.remove('header-scroll');
+    }
+}
+
+//Click menu item -> hide navbar
+
+const navBars = document.querySelectorAll('.nav-link');
+const navCollapse = document.querySelector('.collapse.navbar-collapse');
+
+navBars.forEach((navBar)=>{
+    navBar.addEventListener('click', function(){
+        navCollapse.classList.remove('show');
+    })
+})
+
